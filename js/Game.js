67035f721cf0756ac7478953c8e23cfb38308287
stefanolami <stefanolami90@gmail.com>
2022-03-26 13:14:59 +0100

    started phrase and game classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,55 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        /* this.phrases = ['leave the atom alone',
+                        'the science of today is the technology of tomorrow',
+                        'something incredible is waiting to be known',
+                        'there is no law except the law that there is no law',
+                        'science never solves a problem without creating ten more']; */
+        this.activePhrase = null;
+    }
+
+    createPhrases() {
+
+        const phrasesObjects = [];
+        const phrases = ['leave the atom alone',
+                        'the science of today is the technology of tomorrow',
+                        'something incredible is waiting to be known',
+                        'there is no law except the law that there is no law',
+                        'science never solves a problem without creating ten more'];
+        
+        for (let i = 0; i < phrases.length; i++) {
+            const phraseObj = new Phrase(phrases[i]);
+            phrasesObjects.push(phraseObj);
+        }
+        return phrasesObjects;
+
+    }
+
+    startGame() {
+        document.querySelector("#overlay").style.display = "none";
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    getRandomPhrase() {
+        const i = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[i];
+    }
+
+    removeLife() {
+        const hearts = document.querySelectorAll("img[src='images/liveHeart.png']");
+        const lastHeart = hearts[hearts.length - 1];
+        lastHeart.src = "images/lostHeart.png";
+        this.missed++;
+        if (this.missed >= 5) {
+            this.gameOver();
+        }
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,48 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+        const phraseDiv = document.querySelector("#phrase");
+        const phraseArray = this.phrase.split("");
+        let phraseDivHTML = "";
+        
+        /* for (let i = 0; i < phraseArray.length; i++) {
+            const li = document.createElement("li");
+            if (phraseArray[i] === " ") {
+                li.innerHTML = `<li class="space"> </li>`;
+            } else {
+                li.innerHTML = `<li class="hide letter ${phraseArray[i]}">${phraseArray[i]}</li>`;
+            }
+            phraseDiv.appendChild(li);
+        } */
+
+        /* for (let i = 0; i < phraseArray.length; i++) {
+            const li = document.createElement("li");
+            if (phraseArray[i] === " ") {
+                li.textContent = " ";
+                li.classList.add("space");
+            } else {
+                li.textContent = `${phraseArray[i]}`;
+                li.classList.add(`hide letter ${phraseArray[i]}`);    
+            }
+            phraseDiv.appendChild(li);
+        } */
+
+        for (let i = 0; i < phraseArray.length; i++) {
+            
+            if (phraseArray[i] === " ") {
+                phraseDivHTML += `<li class="space"> </li>`;
+            } else {
+                phraseDivHTML += `<li class="hide letter ${phraseArray[i]}">${phraseArray[i]}</li>`;
+            }
+        }
+        phraseDiv.innerHTML = phraseDivHTML;
+    }
+}
